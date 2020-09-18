712dbe0112f60ae5d6ecba643df4967565cf7f3f
DorkyTay <67008340+DorkyTay@users.noreply.github.com>
2020-09-18 19:41:22 -0400

    Add project files.

---
+++ Interfaces/IProductRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductWebAPI.Models;
+
+namespace ProductWebAPI.Interfaces
+{
+    public interface IProductRepo
+    {
+        IEnumerable<Product> GetProducts();
+
+        Product GetProduct(int id);
+
+        Product InsertProduct(Product product);
+
+        Product UpdateProduct(Product ProductwithChanges);
+
+        Product DeleteProduct(int id);
+        IEnumerable<Product> GetAllProducts();
+    }
+}
+
